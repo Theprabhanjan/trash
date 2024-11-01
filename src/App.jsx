9c6afbc5f5207245af1e1ae11cb9870c925d2989
theprabhanjan <prabhanjandhobale@outlook.com>
2024-11-02 04:47:28 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,8 @@ import SkillsAndExperties from './pages/SkillsAndExperties';
 import LocomotiveScroll from 'locomotive-scroll';
 import 'locomotive-scroll/src/locomotive-scroll.scss';
 import { useEffect } from 'react';
-import MouseFollower from './components/MouseFollower';
-import Loader from './components/Loader';
+
+
 
 
 function App() {
@@ -34,13 +34,9 @@ function App() {
         loader.style.top = "-100%"; // Move loader out of view
     }, 5000);
 }
-const mouseFollower=()=> {
-  window.addEventListener("mousemove", function (det) {
-      document.querySelector("#mouse-follower").style.transform = `translate(${det.clientX}px, ${det.clientY}px)`;
-  });
-}
+
 useEffect(()=>{loaderAnimation()},[])
-useEffect(()=>{mouseFollower()},[])
+
 
   return (
  
